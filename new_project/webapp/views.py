d4914f6630870fc761c27b3ee9aa3a080869add0
Muhammed <muhammed@sendme.ng>
2022-06-26 15:08:38 +0100

    Fully implement RESTful API

--- new_project/webapp/views.py
+++ new_project/webapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse # To return the response
+from django.shortcuts import get_object_or_404  # To return 404 when object does not exist
+from rest_framework.views import APIView  # Normal views can return API data
+from rest_framework.response import Response # get status of a particular response
+from rest_framework import status  # for status
+from .models import employees  # name of my model
+from .serializers import employeesSerializer  # Serializer
+
+# Now create a class-based view which inherits our API views
+class employeeList(APIView):
+
+    def get(self, request):
+        employees1 = employees.objects.all()
+        serializer = employeesSerializer(employees1, many=True)
+        return Response(serializer.data)
+
+    def post(self):
+        pass
